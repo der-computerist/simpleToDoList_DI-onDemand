fa042630452402a964e83bed9b93bdd3ad6b30c3
Enrique Aliaga <e.aliaga.chavez@gmail.com>
2022-11-10 17:57:05 -0500

    Temporarily disable template-defined UI tests

--- ToDoListUITests/ToDoListUITests.swift
+++ ToDoListUITests/ToDoListUITests.swift
@@ -22,7 +22,7 @@ class ToDoListUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func _testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
@@ -31,7 +31,7 @@ class ToDoListUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
+    func _testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
