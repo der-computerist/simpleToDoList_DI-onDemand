cbb2de3d0c0057ae09dd319c8d99879944ddf2e8
Enrique Aliaga <e.aliaga.chavez@gmail.com>
2022-11-18 16:07:02 -0500

    Move `activity(fromIdentifier:)` into a protocol extension
    In `ActivityDataStore`, the implementation of
    `activity(fromIdentifier:)` can be provided by the protocol itself,
    since it doesn't depend on any implementation-specific behavior or data.

--- ToDoList/Models/Repositories/Persistence/ActivityStore/ActivityDataStore.swift
+++ ToDoList/Models/Repositories/Persistence/ActivityStore/ActivityDataStore.swift
@@ -13,8 +13,16 @@ public protocol ActivityDataStore {
     @objc dynamic var activities: [Activity] { get }
     @objc dynamic var activitiesCount: Int { get }
     
-    func activity(fromIdentifier activityID: ActivityID) -> Activity?
     func update(activity: Activity, completion: ((Activity) -> Void)?)
     func delete(activity: Activity, completion: ((Activity) -> Void)?)
     func save() throws
+}
+
+extension ActivityDataStore {
+    
+    public func activity(fromIdentifier activityID: ActivityID) -> Activity? {
+        let filteredActivities = activities.filter({ $0.id == activityID })
+        if filteredActivities.isEmpty { return nil }
+        return filteredActivities.first
+    }
 }
