edadd2a4602899f0e64b30adbe4d18fba337e913
Enrique Aliaga <e.aliaga.chavez@gmail.com>
2022-12-21 15:34:12 -0500

    Add a fake `ActivityDataStore` for testing purposes
    This will allows us to run more deterministic tests.

---
+++ ToDoList/Models/Repositories/Persistence/ActivityStore/Implementations/FakeActivityDataStore.swift
@@ -0,0 +1,47 @@
+//
+//  FakeActivityDataStore.swift
+//  ToDoList
+//
+//  Created by Enrique Aliaga on 11/28/22.
+//
+
+import Foundation
+
+public class FakeActivityDataStore: ActivityDataStore {
+    
+    public func readActivities() -> [Activity] {
+        print("Try to read activities from fake disk...")
+        print("  simulating read action...")
+        return [Activity(name: "Play Forza Horizon 5",
+                         description: "On the Xbox Series X",
+                         status: .pending,
+                         id: UUID().uuidString,
+                         dateCreated: Date()),
+                Activity(name: "Play Super Mario Odyssey",
+                         description: "On the Nintendo Switch",
+                         status: .pending,
+                         id: UUID().uuidString,
+                         dateCreated: Date()),
+                Activity(name: "Play The Last of Us Part I",
+                         description: "On the PlayStation 5",
+                         status: .pending,
+                         id: UUID().uuidString,
+                         dateCreated: Date()),
+                Activity(name: "Play Grand Theft Auto V",
+                         description: "On the Xbox Series X",
+                         status: .pending,
+                         id: UUID().uuidString,
+                         dateCreated: Date()),
+                Activity(name: "Play Metroid Dread",
+                         description: "On the Nintendo Switch",
+                         status: .pending,
+                         id: UUID().uuidString,
+                         dateCreated: Date())]
+    }
+    
+    public func save(activities: [Activity]) throws {
+        print("Try to save activities into fake disk...")
+        print("  simulating save action...")
+        print("  ALL ACTIVITIES SAVED!")
+    }
+}
